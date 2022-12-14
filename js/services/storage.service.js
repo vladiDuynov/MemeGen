9d52ec1dcbd2699d978d4691b9baff0df2f9a613
Vladi Duynov <vladiduynoviesf@gmail.com>
2022-12-14 14:18:52 +0200

    first commit

---
+++ js/services/storage.service.js
@@ -0,0 +1,12 @@
+'use stricts'
+
+console.log('storage service is working')
+
+function saveToStorage(key, val) {
+    localStorage.setItem(key, JSON.stringify(val))
+}
+
+function loadFromStorage(key) {
+    var val = localStorage.getItem(key)
+    return JSON.parse(val)
+}
